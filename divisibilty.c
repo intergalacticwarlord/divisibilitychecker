29556a58bfed1bd308814b17eed0923b5f4bf405
intergalacticwarlord <sapiensneanderathal@gmail.com>
2025-05-20 21:32:28 +0530

    added odd , even check feature , used ternery operators

--- divisibilty.c
+++ divisibilty.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-int main() {
-    int a, b;
-    printf("The number a is: \n");
-    scanf("%d", &a);
-    printf("The number b is: \n");
-    scanf("%d", &b);
-
-    if (a % b == 0) {
-        printf("The number a is divisible by b \n and the quotient is: %d", (a / b));
-    } else {
-        printf("The number a is not divisible by b \n ");
-        printf("The quotient when number a is divided by b is %d \n and the remainder is %d", (a / b), (a % b));
-    }
+int main(){
+  int a ,b ;
+  printf("The no. a is: \n");
+  scanf("%d", &a);
+  printf("The number b is: \n");
+  scanf("%d", &b);
+  a%b==0? printf("a is divisible by b and it's quotient is: \n %d", (a/b)): printf("a is not divisible by b \n The quotient is %d\n And the remainder is : %d",(a/b),(a%b));
+  int c = a%2;
+  switch(c){
+    case 0:
+       printf("\na is an even number");
+       break;
+    case !0:
+       printf("\na is an odd number");
+       break;
+  }
+  return 0;
 }
